1d5d086611afbc60b46d7b1c252572ed16734c56
Bowen Xie <bowen.xie>
2020-05-15 14:12:08 -0600

    a

---
+++ convert.py
@@ -0,0 +1,17 @@
+import base64
+import json
+import os
+
+# define global variables:
+root = "./data"
+all_mask_dir = os.path.join(root, "mask_all1")
+cropped_mask_black = os.path.join(root, "output")
+cropped_mask_white = os.path.join(root, "output_whitebg")
+
+def getRootjson():
+    pass
+
+
+if __name__ == "__main__":
+
+
