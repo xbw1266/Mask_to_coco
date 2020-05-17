66e7e32e88946676c9c3fc656643ff9d42cb8886
Bowen Xie <bowen.xie>
2020-05-16 19:44:04 -0600

    add function to create json for each image

--- convert.py
+++ convert.py
@@ -3,6 +3,21 @@ import json
 import os
 import glob
 import cv2
+import numpy as np
+import functools
+import time
+from tqdm import tqdm
+
+def timer(func):
+	@functools.wraps(func)
+	def wrapper_timer(*args, **kwargs):
+		start_time = time.time()
+		value = func(*args, **kwargs)
+		end_time = time.time()
+		run_time = end_time - start_time
+		print("Finished {} in {} secs".format(func.__name__, run_time))
+		return value
+	return wrapper_timer 	
 
 # define global variables:
 root = "./data"
@@ -24,12 +39,71 @@ def getRootjson():
         im_id += 1
     with open(root_json, 'w') as f:
         json.dump(data, f, indent=4)
-        
-def getImgInfo(img):
+
+
+def getImgInfo(img, img_path):
     assert img is not None, "Image data error!"
-    data = {'version' : '4.2.9', 'flags' : {}, 'shapes' : [], 'imagePath', 'imageData', 'imageHeight', 'imageWidth'}
-    
+    data = {'version' : '4.2.9', 'flags' : {}, 'shapes' : [{'label' : 'baxter', 'points' : [], 'group_id' : None, 'shape_type' : 'polygon', 'flags' : {}}], 'imagePath' : '', 'imageData' : '', 'imageHeight' : '', 'imageWidth' : ''}
+    corners = getCorners(img, 15, False)
+    if corners is not None:
+        for point in corners:
+            data['shapes'][0]['points'].append([int(point[0]), int(point[1])])
+        data['imagePath'] = img_path.split('/')[-1]
+        img_name_no_ext = data['imagePath'].split('.')[0]
+        with open(img_path, 'rb') as f:
+            data['imageData'] = str(base64.b64encode(f.read()))
+        data['imageHeight'] = img.shape[1]
+        data['imageWidth'] = img.shape[0]
+        with open(os.path.join("img_json", img_name_no_ext + ".json"), 'w+') as f:
+            json.dump(data, f, indent=4)
+        return True
+    else:
+        print(img_path)
+        return False
+
+# using Harris corner detection to find corners?
+def getCorners(img, sample_rate=10, show_image=False):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    edge = cv2.Canny(gray, 30, 200)
+    cnts, _ = cv2.findContours(edge, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if len(cnts) == 0:
+        return None
+    output = sort_points(cnts)
+    # print(output.shape)
+    sub_sampled = output[0::sample_rate]
+    if show_image:
+        for point in sub_sampled:
+            print(point)
+            cv2.circle(img, tuple(point), 3, (0, 0, 255), 1)
+        cv2.imshow("ddd", img)
+        cv2.waitKey(30)
+    return sub_sampled
+
+def sort_points(cnts):
+    output = np.squeeze(cnts[0])
+    for cnt in cnts[1:]:
+        new_cnt = np.squeeze(cnt)
+        output = np.vstack((output, new_cnt))
+    return output
+        
+
+def get_corners_from_contours(contours, corner_amount=16):
+    while True:
+        # epsilon = coefficient * cv2.arcLength(contours, True) if coefficient > 0 else 0
+        epsilon = 0.001
+        # print("epsilon:", epsilon)
+        poly_approx = cv2.approxPolyDP(contours, epsilon, True)
+        hull = cv2.convexHull(poly_approx)
+        return hull
 
 if __name__ == "__main__":
-    getRootjson()
-
+    #getRootjson()
+    img = glob.glob(all_mask_dir + "/*.png")
+    for img_path in tqdm(img):
+        img_cv = cv2.imread(img_path)
+        img_name = img_path.split("/")[-1]
+        img_name_no_ext = img_name.split(".")[0]
+        img_cv = img_cv[200:316+690, 100:350+486]
+        save_loc = "./output"
+        if getImgInfo(img_cv, img_path):
+            cv2.imwrite(os.path.join(save_loc, img_name_no_ext + ".jpg"), img_cv)
