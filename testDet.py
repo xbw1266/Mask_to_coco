6bf9be752c5310da649c735888abd678b26e7291
Bowen Xie <bowen.xie@ualberta.ca>
2020-05-19 17:09:10 -0600

    Detectron2 works

---
+++ testDet.py
@@ -0,0 +1,70 @@
+import cv2
+import glob
+from detectron2 import model_zoo
+from detectron2.engine.defaults import DefaultPredictor
+from detectron2.utils.visualizer import ColorMode, Visualizer
+from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog
+from detectron2.data.datasets import register_coco_instances
+import time
+import functools
+import argparse
+import os
+import torch
+
+
+def timer(func):
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.time()
+        value = func(*args, **kwargs)
+        end_time = time.time()
+        run_time = end_time - start_time
+        print("Finished {} in {} secs".format(func.__name__, run_time))
+        return value
+    return wrapper_timer
+
+class DetectronInf():
+    def __init__(self, weights_dir):
+        self.cpu_device = torch.device("cpu")
+        if not os.path.exists(os.path.join(weights_dir, "model_final.pth")):
+            raise FileExistsError("Not found weights!")
+        self.cfg = get_cfg()
+        
+        # self.metadata = MetadataCatalog.get("__unused")
+        register_coco_instances(f"baxter_train", {}, f"baxter/train.json", f"baxter/train")
+        register_coco_instances(f"baxter_test", {}, f"baxter/test.json", f"baxter/test")
+        self.cfg.DATASETS.TEST = ("baxter_test", )
+        self.cfg.DATALOADER.NUM_WORKERS = 2
+        self.cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
+        self.metadata = MetadataCatalog.get("baxter_train")
+        self.cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+        self.cfg.MODEL.WEIGHTS = os.path.join(weights_dir, "model_final.pth")
+        self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.1
+        self.predictor = DefaultPredictor(self.cfg)
+    
+    @timer
+    def test(self, img_path):
+        img = cv2.imread(img_path)
+        # img = img[200:316+690, 100:350+486]
+        assert img is not None
+        output = self.predictor(img)
+        img_vis = img[:, :, ::-1]
+        assert "instances" in output
+        instances = output["instances"].to(self.cpu_device)
+        visualizer = Visualizer(img_vis, self.metadata, scale=0.8, instance_mode=ColorMode.IMAGE )
+        vis_output = visualizer.draw_instance_predictions(instances)
+        rtn = vis_output.get_image()[:, :, ::-1]
+        # rtn = cv2.cvtColor(rtn, cv2.COLOR_RGB2BGR)
+        return rtn
+
+
+if __name__ == "__main__":
+    detector = DetectronInf("./output")
+    # img_path = glob.glob("../new_data/bag1080p/*.jpg")
+    img_path = glob.glob("./swapped_bg_cropped/*.jpg")
+    for img in img_path:
+        cv_img = detector.test(img)
+        cv2.imshow("aaa", cv_img)
+        if cv2.waitKey(0) & 0xff == ord('q'):
+            break
---
+++ update.py
@@ -0,0 +1,79 @@
+import glob
+import os
+import json
+import cv2
+import numpy as np
+from tqdm import tqdm
+
+test_dir = "baxter/test"
+train_dir = "baxter/train"
+root_dir = "baxter"
+
+# define global variables:
+root = "./data"
+all_mask_dir = os.path.join(root, "mask_all1")
+all_mask_filtered_dir = os.path.join(root, "mask_all1_filtered")
+cropped_mask_black = os.path.join(root, "output")
+cropped_mask_white = os.path.join(root, "output_whitebg")
+cropped_swapped_bg = 'swapped_bg_cropped'
+
+
+def getbbox(im_name):
+    mask = cv2.imread(os.path.join(all_mask_dir, im_name + ".png"))[200:316+690, 100:350+486]
+    mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(mask, 0,  1, cv2.THRESH_BINARY)
+    x, y, w, h = cv2.boundingRect(thresh)
+    return x, y, w, h
+
+def getseg(im_name):
+    segmentation = []
+    areas = []
+    mask = cv2.imread(os.path.join(all_mask_dir, im_name + ".png"))[200:316+690, 100:350+486]
+    mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(mask, 0,  1, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours((mask).astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        contour = contour.flatten().tolist()
+        # segmentation.append(contour)
+        if len(contour) > 4:
+            segmentation.append(contour)
+            areas.append(area)
+    assert len(segmentation) != 0
+    max_index = areas.index(max(areas))
+    return segmentation[max_index], areas[max_index]
+
+# json file:
+def getRootjson(impath, json_output):
+    im_id = 0
+    data = {'images':[], 'categories': [{'supercategory' : 'baxter', 'id': 0, 'name': 'baxter'}], 'annotations': []}
+    for img_name in tqdm(glob.glob(impath + "/*.jpg")):
+        img_name_no_ext = img_name.split("/")[-1]
+        img_name_no_ext = img_name_no_ext.split(".")[0]
+        label_info = []
+        # print(img_name)
+        im = cv2.imread(img_name)
+        img_local_name = img_name.split("/")[-1]
+        im_info = {'height': im.shape[0], 'width': im.shape[1], 'id': im_id, 'file_name': img_local_name}
+        data['images'].append(im_info)
+        
+        segmentation_data, area = getseg(img_name_no_ext)
+        # print(area)
+        x, y, w, h = getbbox(img_name_no_ext)
+        label_info ={
+                "segmentation" : [segmentation_data],
+                "area" : area,
+                "iscrowd": 0,
+                "image_id": im_id,
+                "bbox": [x, y, w, h],
+                "category_id": 0,
+                "id": im_id + 1
+            }
+        im_id += 1
+        data['annotations'].append(label_info)
+    with open(json_output, 'w') as f:
+        json.dump(data, f, indent=4)
+
+if __name__ == '__main__':
+    getRootjson(test_dir, "baxter/test.json")
+    getRootjson(train_dir, "baxter/train.json")
